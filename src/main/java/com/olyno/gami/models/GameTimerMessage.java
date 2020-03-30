38752ce33e21264a77967dec002d81751c613788
Olyno <alexlew95@gmail.com>
2020-03-30 17:11:45 +0200

    :sparkles: Implements GameTimerMessage model

---
+++ src/main/java/com/olyno/gami/models/GameTimerMessage.java
@@ -0,0 +1,39 @@
+package com.olyno.gami.models;
+
+import com.olyno.gami.enums.GameMessageTarget;
+
+public class GameTimerMessage extends GameMessage {
+
+    private Integer time;
+
+    /**
+     * Create a special game message with a time
+     * 
+     * @param time The time of the message (when it is showed)
+     * @param messageTarget The target of the message
+     * @param message The message
+     */
+    public GameTimerMessage(Integer time, GameMessageTarget messageTarget, String message) {
+        super(messageTarget, message);
+        this.time = time;
+    }
+
+    /**
+     * Sets the time when the message will be send.
+     * 
+     * @param time The new time of the message
+     */
+    public void setTime(Integer time) {
+        this.time = time;
+    }
+
+    /**
+     * Returns current time of the message
+     * 
+     * @return Time of the message
+     */
+    public Integer getTime() {
+        return time;
+    }
+
+}
