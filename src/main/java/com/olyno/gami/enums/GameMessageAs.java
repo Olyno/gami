8f90d1a21ebf701c8a4663a3a5260e439a7498d0
Olyno <olyno.dev@gmail.com>
2020-08-10 02:50:09 +0200

    :art: Fix GameMessageAs enums

---
+++ src/main/java/com/olyno/gami/enums/GameMessageAs.java
@@ -0,0 +1,25 @@
+package com.olyno.gami.enums;
+
+public enum GameMessageAs {
+
+    /**
+     * Displays as title (default)
+     */
+    TITLE,
+
+    /**
+     * Displays as subtitle
+     */
+    SUBTITLE,
+
+    /**
+     * Displays as action bar message
+     */
+    ACTION_BAR,
+
+    /**
+     * Displays as simple chat message
+     */
+    MESSAGE
+
+}
